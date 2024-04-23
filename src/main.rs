6372f4dfa0c210b21614972b83416322d32ee5a6
chris <chrisbarton124@gmail.com>
2024-04-23 06:00:32 -0700

    can add new rules

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use leptos::*;
-use leptos::ev::Event;
+use leptos::ev::{Event, MouseEvent};
+
+mod linden_system;
 
 fn main() {
     mount_to_body(|| view! { <App/> });
@@ -8,16 +10,10 @@ fn main() {
 #[component]
 fn App() -> impl IntoView {
     view! {
-        <main class="bg-lime-400 h-screen w-screen mx-auto text-center">
+        <main class="main">
             <RuleBox/>
+            <canvas id="canvas"/>
         </main>
-    }
-}
-
-#[component]
-fn LindenCanvas() -> impl IntoView {
-    view! {
-        <canvas class="bg-lime-100"/>
     }
 }
 
@@ -34,16 +30,24 @@ fn RuleBox() -> impl IntoView {
     let (axiom, set_axiom) = create_signal("X".to_string());
     let (angle, set_angle) = create_signal(45.0);
     let (length_factor, set_length_factor) = create_signal(1.2);
-    let (iteration, set_iteration) = create_signal(0);
+
+    let draw_linden_system = move |ev: MouseEvent| {
+        ev.prevent_default();
+    };
 
     view! {
-        <SimpleStringInput input_id="Axiom" reader=axiom writer=set_axiom/>
-        <SimpleFloatInput input_id="Angle" reader=angle writer=set_angle max="360"/>
-        <SimpleFloatInput input_id="Length Factor" reader=length_factor writer=set_length_factor max="10"/>
-        <RuleList/>
+        <div class="rule-box">
+            <SimpleStringInput input_id="Axiom" reader=axiom writer=set_axiom/>
+            <SimpleFloatInput input_id="Angle" reader=angle writer=set_angle max="360"/>
+            <SimpleFloatInput input_id="Length Factor" reader=length_factor writer=set_length_factor max="10"/>
+            <RuleList/>
+            <button on:click=draw_linden_system>"Draw!"</button>
+        </div>
     }
 }
 
+// SimpleStringInput()
+// 
 #[component]
 fn SimpleStringInput(
     input_id: &'static str,
@@ -76,7 +80,7 @@ fn SimpleFloatInput(
         ev.prevent_default();
         match event_target_value(&ev).parse::<f64>() {
             Ok(val) => writer.set(val),
-            Err(e) => writer.set(0.0)
+            Err(_) => writer.set(0.0)
         }
     };
     view! {
@@ -89,7 +93,6 @@ fn SimpleFloatInput(
     }
 }
 
-
 #[derive(Copy, Clone)]
 struct RuleContext(ReadSignal<Vec<(i32, (ReadSignal<String>, WriteSignal<String>), (ReadSignal<String>, WriteSignal<String>))>>);
 #[derive(Copy, Clone)]
@@ -99,36 +102,51 @@ struct SetRuleContext(WriteSignal<Vec<(i32, (ReadSignal<String>, WriteSignal<Str
 fn RuleList() -> impl IntoView {
     let rules = use_context::<RuleContext>().unwrap().0;
     let set_rules = use_context::<SetRuleContext>().unwrap().0;
+
+    let create_new_rule = move |ev: MouseEvent| {
+        ev.prevent_default();
+        let rule_id = match rules.get().last() {
+            Some(p) => p.0 + 1i32,
+            None => 1,
+        };
+        let new_rule =(rule_id,  create_signal("".to_string()), create_signal("".to_string()));
+        set_rules.update(|vec| vec.push(new_rule));
+    };
+
     view! {
-        <ul>
+        <ul class="border-solid border-2 border-indigo-600 p-2">
             <For
                 each=move || rules.get()
                 key=|rule| rule.0
-                children=move |(id, (pred, set_pred),(conv, set_conv))| {
+                children=move |(id, (pred, _set_pred),(conv, _set_conv))| {
                     view! {
                         <li>
                             <input type="text" value=pred/>
                             <input type="text" value=conv/>
-                            <button on:click=move |_| {
-                                set_rules.update(|rules| {
-                                    rules.retain(|(rule_id,(pred_sig, _),(conv_sig, _))| {
+                            <button
+                                class="delete-button"
+                                on:click=move |_| {
+                                    set_rules.update(|rules| {
+                                        rules.retain(|(rule_id,(pred_sig, set_pred_sig),(conv_sig, set_conv_sig))| {
                                         // Must dispose of signals manually or else they will exist until dismount e.g. mem leak.
                                         // "This is only necessary in an example with nested signals like this one"
                                         // source- https://book.leptos.dev/view/04_iteration.html
-                                        if rule_id == &id {
-                                            pred_sig.dispose();
-                                            conv_sig.dispose();
-                                        }
-                                        rule_id != &id
-                                    })
-                                });
-                            }
-                            class="size-6 border-solid border-2 border-indigo-600 font-mono"
-                            >"X"</button>
+                                            if rule_id == &id {
+                                                pred_sig.dispose();
+                                                set_pred_sig.dispose();
+                                                conv_sig.dispose();
+                                                set_conv_sig.dispose();
+                                            }
+                                            rule_id != &id
+                                        })
+                                    });
+                                }
+                            >X</button>
                         </li>
                     }
                 }
             />
+            <button on:click=create_new_rule>New Rule</button>
         </ul>
     }
 }
